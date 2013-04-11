ca841e531de69965b98899bd73852e01448f2af5
Bob Silverberg <bob.silverberg@gmail.com>
2013-04-11 00:44:57 -0400

    Remove unused imports Remove nondestructive mark that is incorrect

--- pages/base_test.py
+++ pages/base_test.py
@@ -4,9 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import requests
-import json
-
 from pages.home_page import MozTrapHomePage
 from pages.create_case_page import MozTrapCreateCasePage
 from pages.manage_cases_page import MozTrapManageCasesPage
